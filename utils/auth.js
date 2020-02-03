7c2ea9f554a148ca3cc5c8e3fc41174cfdd51761
Hoangldp <hoang.le@hiwinsoft.com>
2020-02-03 13:47:37 +0700

    Cookie auth

--- utils/auth.js
+++ utils/auth.js
@@ -4,63 +4,64 @@ import nextCookie from 'next-cookies'
 import cookie from 'js-cookie'
 
 export const login = ({ token }) => {
-  cookie.set('token', token, { expires: 1 })
-  Router.push('/profile')
-}
+  // Create a cookie that expires 1 days from now, valid across the entire site
+  cookie.set('token', token, { expires: 1 });
+  Router.push('/');
+};
 
 export const auth = ctx => {
-  const { token } = nextCookie(ctx)
+  const { token } = nextCookie(ctx);
 
   // If there's no token, it means the user is not logged in.
   if (!token) {
     if (typeof window === 'undefined') {
-      ctx.res.writeHead(302, { Location: '/login' })
-      ctx.res.end()
+      ctx.res.writeHead(302, { Location: '/login' });
+      ctx.res.end();
     } else {
-      Router.push('/login')
+      Router.push('/login');
     }
   }
 
   return token
-}
+};
 
 export const logout = () => {
-  cookie.remove('token')
+  cookie.remove('token');
   // to support logging out from all windows
-  window.localStorage.setItem('logout', Date.now())
-  Router.push('/login')
-}
+  window.localStorage.setItem('logout', Date.now());
+  Router.push('/login');
+};
 
 export const withAuthSync = WrappedComponent => {
   const Wrapper = props => {
     const syncLogout = event => {
       if (event.key === 'logout') {
-        console.log('logged out from storage!')
-        Router.push('/login')
+        console.log('logged out from storage!');
+        Router.push('/login');
       }
-    }
+    };
 
     useEffect(() => {
-      window.addEventListener('storage', syncLogout)
+      window.addEventListener('storage', syncLogout);
 
       return () => {
-        window.removeEventListener('storage', syncLogout)
-        window.localStorage.removeItem('logout')
+        window.removeEventListener('storage', syncLogout);
+        window.localStorage.removeItem('logout');
       }
-    }, [])
+    }, []);
 
     return <WrappedComponent {...props} />
-  }
+  };
 
   Wrapper.getInitialProps = async ctx => {
-    const token = auth(ctx)
+    const token = auth(ctx);
 
     const componentProps =
       WrappedComponent.getInitialProps &&
-      (await WrappedComponent.getInitialProps(ctx))
+      (await WrappedComponent.getInitialProps(ctx));
 
     return { ...componentProps, token }
-  }
+  };
 
   return Wrapper
-}
+};
